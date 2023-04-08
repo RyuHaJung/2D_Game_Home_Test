9cea6d654e0dfac2a978c492b12900caec8ce1b6
RyuHaJung <red061011@gmail.com>
2023-04-08 21:35:54 +0900

    미로 게임 UI 배치

---
+++ Test1/Assets/02.Scripts/01.Scene/ToEMaze.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToEMaze : MonoBehaviour
+{
+    public void Change()
+    {
+        SceneManager.LoadScene("04.EMazeGame");
+    }
+
+
+}
---
+++ Test1/Assets/02.Scripts/01.Scene/ToHMaze.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToHMaze : MonoBehaviour
+{
+    public void Change()
+    {
+        SceneManager.LoadScene("06.HMazeGame");
+    }
+
+
+}
