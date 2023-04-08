8b0f7bf7937f1bfce95c6c76e3f736e954f8a0cd
RyuHaJung <red061011@gmail.com>
2023-04-08 18:09:58 +0900

    씬 추가

---
+++ Test1/Assets/02.Scripts/01.Scene/ToHamsterMazeScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToHamsterMazeScene : MonoBehaviour
+{ 
+    public void Change()
+    {
+        SceneManager.LoadScene("03.HamstarMaze");
+    }
+
+
+}
