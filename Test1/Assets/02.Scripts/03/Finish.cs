0e1150bf98c2f41362b0b69272666bd349f83e2d
RyuHaJung <red061011@gmail.com>
2023-04-09 02:58:57 +0900

    햄스터 미로 게임 완성

--- Test1/Assets/02.Scripts/03/Finish.cs
+++ Test1/Assets/02.Scripts/03/Finish.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 public class Finish : MonoBehaviour
 {
     [SerializeField] Text finText;
+    [SerializeField] Button BackButton;
 
     // Start is called before the first frame update
     void Start()
     {
         finText.gameObject.SetActive(false);
+        BackButton.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         finText.gameObject.SetActive(true);
+        ItemSound.FinishSound();
+
+        BackButton.gameObject.SetActive(true);
         ItemSound.FinishSound();
     }
 
