87ff065592f0291c71ceaa6ee18d5be0dfba0f57
RyuHaJung <red061011@gmail.com>
2023-04-15 03:13:58 +0900

    씬 이름 바꿈, 스크립트 점검

--- Test1/Assets/02.Scripts/07~10/GameManager.cs
+++ Test1/Assets/02.Scripts/07~10/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public float timeLimit = 10f;
+    public float timeLimit = 30f;
     private float timeElapsed = 0f;
     // Start is called before the first frame update
     void Start()
